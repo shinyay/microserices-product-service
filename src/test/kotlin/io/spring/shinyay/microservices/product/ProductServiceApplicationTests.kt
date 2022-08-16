baf0752e532dd2a6c62e775eb6ad68b3bccfc413
shinyay <shinya.com@gmail.com>
2022-08-16 14:34:56 +0900

    Initial Commit

---
+++ src/test/kotlin/io/spring/shinyay/microservices/product/ProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.spring.shinyay.microservices.product
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
